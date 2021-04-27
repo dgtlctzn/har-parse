46d047b7ebcd0aca7a0a65677b758ea021fee21e
Joseph Perry <josephperry720@gmail.com>
2021-04-26 20:43:31 -0400

    added text content to options

--- har_parse.py
+++ har_parse.py
@@ -1,7 +1,6 @@
 import json
-import sys
-from typing import List
 from pprint import pprint
+from typing import List
 
 from haralyzer import HarParser
 from PyInquirer import prompt
@@ -26,18 +25,14 @@ def format_question(_type: str, message: str, name: str, choices=None) -> List[d
     return question
 
 
-def clean_headers(raw_headers: list) -> dict:
-    headers = {}
-    for header in raw_headers:
-        headers[header['name']] = header['value']
-    return headers
-
-
-def clean_cookies(raw_cookies: list) -> dict:
-    cookies = {}
-    for cookie in raw_cookies:
-        cookies[cookie['name']] = cookie['value']
-    return cookies
+def format_to_python(raw: list, pretty=True) -> None:
+    py_format = {}
+    for entry in raw:
+        py_format[entry['name']] = entry['value']
+    if pretty:
+        pprint(py_format)
+    else:
+        print(py_format)
 
 
 def main():
@@ -47,22 +42,38 @@ def main():
             raise ValueError('File must be a .har file')
         hars = [{'name': val} for val in to_har(har_file.get('har_file'))[0].entries]
         har_choices = [{'name': str(val)} for val in to_har(har_file.get('har_file'))[0].entries]
-        ans = prompt(format_question('list', 'which request?', 'har', choices=har_choices))
+        ans = prompt(format_question('list', 'Which request?', 'har', choices=har_choices))
         chosen_har = hars[har_choices.index({'name': ans.get('har')})]
-        res_or_req = prompt(format_question('list', 'request or response?', 'res_or_req', choices=[{'name': 'response'}, {'name': 'request'}]))
-        get = prompt(format_question('list', 'what do you want to get?', 'type', choices=[{'name': 'headers'}, {'name': 'cookies'}]))
+        res_or_req = prompt(
+            format_question(
+                'list',
+                'Request or response?',
+                'res_or_req',
+                choices=[{'name': 'response'}, {'name': 'request'}]
+            )
+        )
+        info_choices = [{'name': 'headers'}, {'name': 'cookies'}]
+        if res_or_req.get('res_or_req') == 'response':
+            info_choices.append({'name': 'text'})
+        info_choice = prompt(format_question('list', 'What do you want to get?', 'type', choices=info_choices))
+        pprint_choice = prompt(format_question('confirm', 'Pretty print?', 'pretty'))
 
         if res_or_req.get('res_or_req') == 'request':
-            if get.get('type') == 'headers':
-                pprint(clean_headers(chosen_har.get('name').request.headers))
-            elif get.get('type') == 'cookies':
-                pprint(clean_cookies(chosen_har.get('name').request.cookies))
+            if info_choice.get('type') == 'headers':
+                format_to_python(chosen_har.get('name').request.headers, pprint_choice.get('pretty'))
+            elif info_choice.get('type') == 'cookies':
+                format_to_python(chosen_har.get('name').request.cookies, pprint_choice.get('pretty'))
         else:
-            if get.get('type') == 'headers':
-                pprint(clean_headers(chosen_har.get('name').response.headers))
-            elif get.get('type') == 'cookies':
-                pprint(clean_cookies(chosen_har.get('name').response.cookies))
-    except FileNotFoundError as fe:
+            if info_choice.get('type') == 'headers':
+                format_to_python(chosen_har.get('name').response.headers, pprint_choice.get('pretty'))
+            elif info_choice.get('type') == 'cookies':
+                format_to_python(chosen_har.get('name').response.cookies, pprint_choice.get('pretty'))
+            elif info_choice.get('type') == 'text':
+                if pprint_choice.get('pretty'):
+                    pprint(chosen_har.get('name').response.text)
+                else:
+                    print(chosen_har.get('name').response.text)
+    except FileNotFoundError:
         raise Exception("File does not exist")
 
 
